94afda88c181a8f790dd1d8589b8ca5c544d7efb
gulshangitcoder <gulshankr000212@gmail.com>
2023-02-05 00:34:39 +0530

    Complex Pattern

---
+++ ComplexPattern.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ComplexPattern {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter any number: ");
+    int n = sc.nextInt();
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            if(i==0 || j==0 || i==n-1 || j==n-1 || i==0 && j<=(n-1)/2 || i+j<=(n-1)/2 || i-j>=(n-1)/2 || j-i>=(n-1)/2 || i+j>=(n-1)+(n-1)/2)
+            {
+                System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+
+        }
+        System.out.println();
+    }   
+    }
+    
+}
